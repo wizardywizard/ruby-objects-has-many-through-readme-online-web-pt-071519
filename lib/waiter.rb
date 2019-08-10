e1e33ce91fc3ba94949177fa5764dadcbdddb7c7
David Little <littledavid175@gmail.com>
2019-08-10 17:13:06 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,7 +17,11 @@ attr_accessor :name, :yrs_experience
     Meal.new(self, customer, total, tip)
   end
   
-  
+  def meals
+  Meal.all.select do |meal|
+    meal.waiter == self 
+  end
+end
   
   
 end
